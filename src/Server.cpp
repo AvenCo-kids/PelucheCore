33dbeba86a698087acc95166a6b5475f756cd80e
Quentin Veyrenc <quentin.veyrenc@epitech.eu>
2020-12-03 11:44:04 +0100

    feat: added a draft of class that will run the peluche's server (to remotely start/stop a story)

---
+++ src/Server.cpp
@@ -0,0 +1,5 @@
+//
+// Created by quentin on 03/12/2020.
+//
+
+#include "Server.h"
---
+++ src/Server.h
@@ -0,0 +1,10 @@
+#ifndef AVENCO_PC_PELUCHE_H_
+#define AVENCO_PC_PELUCHE_H_
+
+#include "Coordinator.h"
+
+namespace APC {
+    class Server {
+        // Expose "/start" & "/stop"
+    };
+}
