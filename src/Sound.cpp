9536150de474baffda5b8525b314c523f4c529e2
Sobihan <sobihan.mahedran@epitech.eu>
2020-12-07 17:16:48 +0100

    [ADDED] Audio

---
+++ src/Sound.cpp
@@ -0,0 +1,45 @@
+/*
+** EPITECH PROJECT, 2020
+** sound
+** File description:
+** Sound
+*/
+
+#include "Sound.hpp"
+
+namespace APC
+{
+
+    /* CTOR & DTOR */
+
+    Sound::Sound(const std::string &path, bool repeat) { setSound(path, repeat); }
+
+    /* PUBLIC METHODS */
+
+    bool Sound::setSound(const std::string &path, bool repeat)
+    {
+        if (!_mus.openFromFile(path.c_str()))
+            return error();
+        if (repeat)
+            _mus.setLoop(repeat);
+        return true;
+    }
+
+    void Sound::play()
+    {
+        std::cout << _mus.getVolume() << std::endl;
+        _mus.play();
+    }
+
+    void Sound::stop() { _mus.stop(); }
+
+    void Sound::pause() { _mus.pause(); }
+
+    bool Sound::error()
+    {
+        std::cerr << "\033m[ERROR] [AudiOut] [Sound]"
+                  << " File doesn't exist\033[0m" << std::endl;
+        return false;
+    }
+
+} // namespace APC
