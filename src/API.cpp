e3f6053e04d075bb5fc917037842510da32bb8a4
Sobihan <sobihan.mahedran@epitech.eu>
2020-12-07 12:30:50 +0100

    [ADDED] get requests

---
+++ src/API.cpp
@@ -0,0 +1,84 @@
+/*
+** EPITECH PROJECT, 2020
+** 
+** File description:
+** API
+*/
+
+#include "API.hpp"
+namespace APC
+{
+    API::API()
+    {
+        _curl = curl_easy_init();
+    }
+
+    API::~API()
+    {
+        if (_curl)
+        {
+            curl_easy_cleanup(_curl);
+        }
+    }
+
+    void API::initCurl()
+    {
+        if (!_curl)
+            curl_easy_init();
+    }
+
+    static size_t WriteCallback(void *contents, size_t size, size_t nmemb, void *userp)
+    {
+        ((std::string *)userp)->append((char *)contents, size * nmemb);
+        return size * nmemb;
+    }
+
+    std::pair<std::string, long> API::getRequest(std::string url)
+    {
+        std::string buffer;
+        CURLcode res;
+        long httpCode;
+
+        initCurl();
+        curl_easy_setopt(_curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(_curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(_curl, CURLOPT_WRITEDATA, &buffer);
+        res = curl_easy_perform(_curl);
+        curl_easy_getinfo(_curl, CURLINFO_RESPONSE_CODE, &httpCode);
+        if (httpCode != 200)
+            buffer = "[Error]";
+        return std::make_pair(buffer, httpCode);
+    }
+
+    std::pair<std::string, long> API::launchStory(std::string id, std::string storyID)
+    {
+        std::string url = _url;
+
+        url += "story/launch/";
+        url += id;
+        url += "/";
+        url += storyID;
+
+        return getRequest(url);
+    }
+
+    std::pair<std::string, long> API::getStory(std::string storyID)
+    {
+        std::string url = _url;
+
+        url += "story/";
+        url += storyID;
+        return getRequest(url);
+    }
+
+    std::pair<std::string, long> API::listStories(std::string limit, std::string skip)
+    {
+        std::string url = _url;
+
+        url += "story?limit=";
+        url += limit;
+        url += "&skip=";
+        url += skip;
+        return getRequest(url);
+    }
+} // namespace APC
---
+++ src/API.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2020
+** 
+** File description:
+** API
+*/
+
+#ifndef API_HPP_
+#define API_HPP_
+
+#include <curl/curl.h>
+#include <string>
+
+namespace APC
+{
+    class API
+    {
+    public:
+        API();
+        ~API();
+        std::pair<std::string, long> launchStory(std::string id, std::string storyID);
+        std::pair<std::string, long> getStory(std::string storyID);
+        std::pair<std::string, long> listStories(std::string limit, std::string skip);
+
+    protected:
+    private:
+        void initCurl();
+        std::pair<std::string, long> getRequest(std::string url);
+
+    private:
+        std::string _url = "http://localhost:3000/";
+        CURL *_curl;
+    };
+} // namespace APC
+#endif /* !API_HPP_ */
