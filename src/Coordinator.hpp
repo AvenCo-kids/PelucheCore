95e4f83a6461806ad1862d3b81a9ce8ff7390258
Quentin DI MEO <QDM@localhost.localdomain>
2020-12-07 17:04:57 +0100

    [UPD] src: updating coordinator according to wiki

--- src/Coordinator.hpp
+++ src/Coordinator.hpp
@@ -1,31 +1,31 @@
 #ifndef AVENCO_PC_COORDINATOR_HPP_
 #define AVENCO_PC_COORDINATOR_HPP_
 
-#include <vector>
+#include <map>
 #include <string>
+#include <vector>
 
 namespace APC {
-
     class Coordinator {
     private:
         using Node = struct node_s {
-            char *_mp3; // TODO: find appropriate way to store
-            std::vector<std::string> _annotations; // TODO: find appropriate way to store
+            char *_mp3path;
+            int _timeCode;
+            std::pair<bool exists, std::string script> _script;
             struct node_s *children;
         };
     public:
         Coordinator() = default;
         ~Coordinator() = default;
 
-    public:
         bool getNewStory() noexcept;
         void makeChoice() noexcept;
         bool saveProgress() const noexcept;
-
     private:
+        char *_storyName;
         Node *_storyHead;
         Node *_curr;
-        // some sort of inventory
+        std::map<std::string obj, bool used> _inv;
     };
 }
 
