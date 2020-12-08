62ee307444c8f5e2e1c5e07e648afdf7f5356562
Quentin DI MEO <QDM@localhost.localdomain>
2020-12-08 17:00:46 +0100

    [UPD] tests: fixing tostrings & includes

--- tests/TestsAPI.cpp
+++ tests/TestsAPI.cpp
@@ -1,5 +1,6 @@
 #include <criterion/criterion.h>
 #include <curl/curl.h>
+//#include <string>
 #include "../src/API.hpp"
 
 Test(api, initialization)
@@ -11,41 +12,41 @@ Test(api, initialization)
 Test(api, get_list_of_stories)
 {
     APC::API api;
-    auto r = api.listStories(to_string(0), to_string(10));
+    auto r = api.listStories("0", "10");
     cr_assert(r.second == 200, "Failed to gather the list of stories.");
 }
 
 Test(api, get_list_of_stories_invalid_params)
 {
     APC::API api;
-    auto r = api.listStories(to_string(1), to_string(-1));
+    auto r = api.listStories("1", "-1");
     cr_assert(r.second >= 300, "Succeeded upon negative list size.");
 }
 
 Test(api, request_story)
 {
     APC::API api;
-    auto r = api.getStory(to_string(0 /* existing ID */));
+    auto r = api.getStory("0" /* existing ID */);
     cr_assert(r.second == 200, "Couldn't get the requested story.");
 }
 
 Test(api, request_story_nonexistent)
 {
     APC::API api;
-    auto r = api.getStory(to_string(0xDEADBEEF));
+    auto r = api.getStory(std::to_string(0xDEADBEEF));
     cr_assert(r.second >= 300, "Got Success on a nonexistent story ID.");
 }
 
 Test(api, launch_story)
 {
     APC::API api;
-    auto r = api.launchStory(to_string(0), to_string(0) /* existing IDs */);
+    auto r = api.launchStory("0", "0" /* existing IDs */);
     cr_assert(r.second == 200, "Couldn't start the requested story.");
 }
 
 Test(api, launch_story_nonexistent)
 {
     APC::API api;
-    auto r = api.launchStory(to_string(0), to_string(0xDEADBEEF));
+    auto r = api.launchStory("0", std::to_string(0xDEADBEEF));
     cr_assert(r.second >= 300, "Succeeded to launch a nonexistent story.");
 }
